c170f445d7157b6d6204f43ccada20c0c81c7d1e
rcueto23 <rcuetosolano23@gmail.com>
2024-04-19 09:09:14 -0500

    m2

--- src/app/components/contenedor/Inicio.tsx
+++ src/app/components/contenedor/Inicio.tsx
@@ -9,21 +9,21 @@ export const Inicio = () => {
             <div className="d-flex text-body-secondary pt-3">
             <svg className="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#007bff"></rect><text x="50%" y="50%" fill="#007bff" dy=".3em">32x32</text></svg>
             <p className="pb-3 mb-0 small lh-sm border-bottom">
-                <strong className="d-block text-gray-dark">Nueva temporada de Kimetsu No Yaiba</strong>
+                <strong className="d-block text-gray-dark">Nueva temporada de Kimetsu No Yaiba.</strong>
                 Se anunció nueva temporada del famoso anime Kimetsu No Yaiba, que contara con 24 capitulos y una pelicula y que abarcara el arco del tren infinito. 
             </p>
             </div>
             <div className="d-flex text-body-secondary pt-3">
             <svg className="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#e83e8c"></rect><text x="50%" y="50%" fill="#e83e8c" dy=".3em">32x32</text></svg>
             <p className="pb-3 mb-0 small lh-sm border-bottom">
-                <strong className="d-block text-gray-dark">Se cancela anime de Tokyo Revengers</strong>
+                <strong className="d-block text-gray-dark">Se cancela anime de Tokyo Revengers.</strong>
                 Luego de muchos capitulos la casa animadora de Tokyo Revengers acaba de anunciar que no van a continuar animando esta gran obra.
             </p>
             </div>
             <div className="d-flex text-body-secondary pt-3">
             <svg className="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#6f42c1"></rect><text x="50%" y="50%" fill="#6f42c1" dy=".3em">32x32</text></svg>
             <p className="pb-3 mb-0 small lh-sm border-bottom">
-                <strong className="d-block text-gray-dark">Remake de Tokyo Ghoul</strong>
+                <strong className="d-block text-gray-dark">Remake de Tokyo Ghoul.</strong>
                 Se acaba de anunciar que el anime Tokyo Revenger tendra un remake y que sera 100% fiel al manga, ademas contara con dos peliculas.
             </p>
             </div>
